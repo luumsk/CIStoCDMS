127eedd2892d1491fe8a663d3509abf4b482a28b
khue <luumsk@gmail.com>
2024-08-11 16:17:39 +0700

    move all model data to results folder

--- utils/config.py
+++ utils/config.py
@@ -14,32 +14,32 @@ y_val_paths   = [f'data/val/y_val_fold{fold+1}.csv' for fold in range(5)]
 
 paths = {
     'catboost': {
-        'best_params': 'data/catboost/best_params.json',
-        'pred_5folds': 'data/catboost/predictions_5folds.csv',
-        'models': [f'data/catboost/models/fold{fold+1}.cbm' for fold in range(5)],
-        'shap_fi': 'data/catboost/shap_feature_importance.csv',
-        'feature_importance': 'data/catboost/feature_importance.csv'
+        'best_params': 'results/catboost/best_params.json',
+        'pred_5folds': 'results/catboost/predictions_5folds.csv',
+        'models': [f'results/catboost/models/fold{fold+1}.cbm' for fold in range(5)],
+        'shap_fi': 'results/catboost/shap_feature_importance.csv',
+        'feature_importance': 'results/catboost/feature_importance.csv'
     },
     'xgboost': {
-        'best_params': 'data/xgboost/best_params.json',
-        'pred_5folds': 'data/xgboost/predictions_5folds.csv',
-        'models': [f'data/xgboost/models/fold{fold+1}.json' for fold in range(5)],
-        'shap_fi': 'data/xgboost/shap_feature_importance.csv',
-        'feature_importance': 'data/xgboost/feature_importance.csv'
+        'best_params': 'results/xgboost/best_params.json',
+        'pred_5folds': 'results/xgboost/predictions_5folds.csv',
+        'models': [f'results/xgboost/models/fold{fold+1}.json' for fold in range(5)],
+        'shap_fi': 'results/xgboost/shap_feature_importance.csv',
+        'feature_importance': 'results/xgboost/feature_importance.csv'
     },
     'lgbm': {
-        'best_params': 'data/lgbm/best_params.json',
-        'pred_5folds': 'data/lgbm/predictions_5folds.csv',
-        'models': [f'data/lgbm/models/fold{fold+1}.joblib' for fold in range(5)],
-        'shap_fi': 'data/lgbm/shap_feature_importance.csv',
-        'feature_importance': 'data/lgbm/feature_importance.csv'
+        'best_params': 'results/lgbm/best_params.json',
+        'pred_5folds': 'results/lgbm/predictions_5folds.csv',
+        'models': [f'results/lgbm/models/fold{fold+1}.joblib' for fold in range(5)],
+        'shap_fi': 'results/lgbm/shap_feature_importance.csv',
+        'feature_importance': 'results/lgbm/feature_importance.csv'
     },
     'rf': {
-        'best_params': 'data/rf/best_params.json',
-        'pred_5folds': 'data/rf/predictions_5folds.csv',
-        'models': [f'data/rf/models/fold{fold+1}.joblib' for fold in range(5)],
-        'shap_fi': 'data/rf/shap_feature_importance.csv',
-        'feature_importance': 'data/rf/feature_importance.csv'
+        'best_params': 'results/rf/best_params.json',
+        'pred_5folds': 'results/rf/predictions_5folds.csv',
+        'models': [f'results/rf/models/fold{fold+1}.joblib' for fold in range(5)],
+        'shap_fi': 'results/rf/shap_feature_importance.csv',
+        'feature_importance': 'results/rf/feature_importance.csv'
     },
     'svm': {
         'best_params': 'data/svm/best_params.json',
@@ -48,9 +48,9 @@ paths = {
         'shap_fi': 'data/svm/shap_feature_importance.csv',
     },
     'lr': {
-        'best_params': 'data/lr/best_params.json',
-        'pred_5folds': 'data/lr/predictions_5folds.csv',
-        'models': [f'data/lr/models/fold{fold+1}.joblib' for fold in range(5)],
-        'shap_fi': 'data/lr/shap_feature_importance.csv',
+        'best_params': 'results/lr/best_params.json',
+        'pred_5folds': 'results/lr/predictions_5folds.csv',
+        'models': [f'results/lr/models/fold{fold+1}.joblib' for fold in range(5)],
+        'shap_fi': 'results/lr/shap_feature_importance.csv',
     }
 }

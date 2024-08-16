440f65f261b1960eebf725151517e2ac5d1a1545
khue <luumsk@gmail.com>
2024-08-16 15:26:34 +0700

    update predict notebook + add prediction files for each model

--- src/paths.py
+++ src/paths.py
@@ -30,8 +30,10 @@ for model_name in MODEL_NAMES:
         'best_params': os.path.join(model_dir, 'best_params.json'),
         'pred_5folds': os.path.join(model_dir, 'predictions_5folds.csv'),
         'shap_fi'    : os.path.join(model_dir, 'shap_feature_importance.csv'),
-        'sv': os.path.join(model_dir, 'sv.json'),
-        'iv': os.path.join(model_dir, 'iv.json')
+        'sv'         : os.path.join(model_dir, 'sv.json'),
+        'iv'         : os.path.join(model_dir, 'iv.json'),
+        'pred'       : os.path.join(model_dir, 'pred.json'),
+        'pred_proba' : os.path.join(model_dir, 'pred_proba.json'),
     }
 
     # Catboost model file ending is .cbm and this classifier has feature importance
